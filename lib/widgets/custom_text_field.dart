c4312b27f0a9dbb20aedc87b7a183544aca914fe
raedthawaba <raed01560@gmail.com>
2026-02-20 20:25:32 +0300

    Add lib/widgets/custom_text_field.dart

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../utils/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String? hintText;
+  final IconData prefixIcon;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final int maxLines;
+  final bool enabled;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    this.hintText,
+    required this.prefixIcon,
+    this.suffixIcon,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.validator,
+    this.onChanged,
+    this.maxLines = 1,
+    this.enabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+      onChanged: onChanged,
+      maxLines: maxLines,
+      enabled: enabled,
+      style: GoogleFonts.tajawal(
+        fontSize: 16,
+        color: AppColors.textPrimary,
+      ),
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        hintStyle: GoogleFonts.tajawal(
+          fontSize: 14,
+          color: AppColors.textSecondary,
+        ),
+        labelStyle: GoogleFonts.tajawal(
+          fontSize: 14,
+          color: AppColors.textSecondary,
+        ),
+        prefixIcon: Icon(
+          prefixIcon,
+          color: AppColors.primary,
+          size: AppDimensions.iconSize,
+        ),
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: enabled ? AppColors.surface : AppColors.background,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: AppDimensions.paddingMedium,
+          vertical: AppDimensions.paddingMedium,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+          borderSide: const BorderSide(color: AppColors.divider),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+          borderSide: const BorderSide(color: AppColors.divider),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+          borderSide: const BorderSide(color: AppColors.primary, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+          borderSide: const BorderSide(color: AppColors.error),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(AppDimensions.borderRadius),
+          borderSide: const BorderSide(color: AppColors.error, width: 2),
+        ),
+        errorStyle: GoogleFonts.tajawal(
+          fontSize: 12,
+          color: AppColors.error,
+        ),
+      ),
+    );
+  }
+}
